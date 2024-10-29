3757a91e4557e419ab2a9b25226c758cf5b5098a
enszkyle <kyleensz1@gmail.com>
2024-10-29 12:30:56 -0500

    Add files via upload

---
+++ Login.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace LNB_Airlines_Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdEmployee = new System.Windows.Forms.RadioButton();
+            this.rdAdmin = new System.Windows.Forms.RadioButton();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(136, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LNB Airlines";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(167, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Secure Login Portal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Select Role";
+            // 
+            // rdEmployee
+            // 
+            this.rdEmployee.AutoSize = true;
+            this.rdEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdEmployee.Location = new System.Drawing.Point(128, 161);
+            this.rdEmployee.Name = "rdEmployee";
+            this.rdEmployee.Size = new System.Drawing.Size(92, 22);
+            this.rdEmployee.TabIndex = 3;
+            this.rdEmployee.TabStop = true;
+            this.rdEmployee.Text = "Employee";
+            this.rdEmployee.UseVisualStyleBackColor = true;
+            this.rdEmployee.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdAdmin
+            // 
+            this.rdAdmin.AutoSize = true;
+            this.rdAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdAdmin.Location = new System.Drawing.Point(242, 161);
+            this.rdAdmin.Name = "rdAdmin";
+            this.rdAdmin.Size = new System.Drawing.Size(67, 22);
+            this.rdAdmin.TabIndex = 4;
+            this.rdAdmin.TabStop = true;
+            this.rdAdmin.Text = "Admin";
+            this.rdAdmin.UseVisualStyleBackColor = true;
+            this.rdAdmin.CheckedChanged += new System.EventHandler(this.rdAdmin_CheckedChanged);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(31, 215);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(60, 15);
+            this.lblUsername.TabIndex = 5;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(31, 300);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(57, 15);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUsername.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtUsername.Location = new System.Drawing.Point(34, 233);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(380, 50);
+            this.txtUsername.TabIndex = 7;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(34, 318);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(380, 49);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogIn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.ForeColor = System.Drawing.Color.White;
+            this.btnLogIn.Location = new System.Drawing.Point(34, 403);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(380, 41);
+            this.btnLogIn.TabIndex = 9;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(455, 544);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.rdAdmin);
+            this.Controls.Add(this.rdEmployee);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdEmployee;
+        private System.Windows.Forms.RadioButton rdAdmin;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogIn;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;  // adding an Access Object
+
+namespace LNB_Airlines_Login
+{
+    public partial class Login : Form
+    {
+
+        // Create SQL Application
+        private SqlConnection sqlConnection;
+        private string connectionString = @"Data Source=DESKTOP-UPSRTGM\SQLEXPRESS;Initial Catalog=LNB_AirlinesDB;Integrated Security=True;TrustServerCertificate=True";
+
+
+        public Login()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(connectionString);
+
+        }
+
+        private bool ValidateUser(string username, string password, string role)
+        {
+            // Open the SQL connection
+            sqlConnection.Open();
+
+            // Update the query to check against the Users table and include role
+            string query = @"SELECT COUNT(*) AS UserCount 
+                     FROM Users 
+                     WHERE Username = @username 
+                     AND Password = @password 
+                     AND Role = @role";
+
+            // Create and configure the SQL command
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+            command.Parameters.AddWithValue("@username", username);
+            command.Parameters.AddWithValue("@password", password);
+            command.Parameters.AddWithValue("@role", role);
+
+            // Execute the query and get the user count
+            int userCount = (int)command.ExecuteScalar();
+
+            // Close the connection
+            sqlConnection.Close();
+
+            // Return true if a user is found, false otherwise
+            return userCount > 0;
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            // Step 1: Check if a role is selected
+            if (!rdEmployee.Checked && !rdAdmin.Checked)
+            {
+                MessageBox.Show("Please select a valid role type."); // Debug message
+                return;
+            }
+
+            // Step 2: Determine the selected role
+            string userSelectedRole = rdEmployee.Checked ? "Employee" : "Admin";
+            //MessageBox.Show($"Role selected: {userSelectedRole}"); // Debug message
+
+            // Step 3: Validate the user credentials using the ValidateUser method
+            bool isValidUser = ValidateUser(txtUsername.Text, txtPassword.Text, userSelectedRole);
+            //MessageBox.Show($"User valid: {isValidUser}"); // Debug message
+
+            if (isValidUser)
+            {
+                // Step 4: User role is correct, proceed based on the role
+                if (userSelectedRole == "Admin")
+                {
+                    //MessageBox.Show("Navigating to Admin Form"); // Debug message
+                    AdminForm adminForm = new AdminForm(); // Instantiate the AdminForm
+                    adminForm.Show(); // Show the AdminForm
+                    this.Hide(); // Hide the login form
+                }
+                else if (userSelectedRole == "Employee")
+                {
+                    //MessageBox.Show("Navigating to Employee Form"); // Debug message
+                    EmployeeForm employeeForm = new EmployeeForm(); // Instantiate the EmployeeForm
+                    employeeForm.Show(); // Show the EmployeeForm
+                    this.Hide(); // Hide the login form
+                }
+            }
+            else
+            {
+                // Step 5: Handle incorrect username or password
+                MessageBox.Show("Incorrect username or password!"); // Debug message
+                txtPassword.Clear();
+                txtUsername.Clear();
+                txtUsername.Focus();
+            }
+        }
+
+        private void rdAdmin_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
