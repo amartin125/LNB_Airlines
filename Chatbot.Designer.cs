26495b481099086a976762d057554a44cdc5d94a
Kyle Ensz <kte@ksu.edu>
2024-11-11 18:20:51 -0600

    Integrated Chatbot

--- Chatbot.Designer.cs
+++ Chatbot.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace LNB_Airlines
+﻿namespace Chatbot
 {
     partial class Chatbot
     {
@@ -28,33 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnExit = new System.Windows.Forms.Button();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.rtbChatHistory = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnExit
+            // txtUserInput
             // 
-            this.btnExit.Location = new System.Drawing.Point(539, 243);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(89, 56);
-            this.btnExit.TabIndex = 1;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.txtUserInput.Location = new System.Drawing.Point(47, 462);
+            this.txtUserInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(532, 22);
+            this.txtUserInput.TabIndex = 0;
             // 
-            // Chatbot
+            // btnSend
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.btnSend.AutoEllipsis = true;
+            this.btnSend.Location = new System.Drawing.Point(575, 459);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 28);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // rtbChatHistory
+            // 
+            this.rtbChatHistory.Location = new System.Drawing.Point(31, 68);
+            this.rtbChatHistory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rtbChatHistory.Name = "rtbChatHistory";
+            this.rtbChatHistory.ReadOnly = true;
+            this.rtbChatHistory.Size = new System.Drawing.Size(643, 368);
+            this.rtbChatHistory.TabIndex = 2;
+            this.rtbChatHistory.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(297, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LNB Chatbot";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnExit);
-            this.Name = "Chatbot";
-            this.Text = "Chatbot";
+            this.ClientSize = new System.Drawing.Size(704, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbChatHistory);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtUserInput);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox rtbChatHistory;
+        private System.Windows.Forms.Label label1;
     }
 }
+
